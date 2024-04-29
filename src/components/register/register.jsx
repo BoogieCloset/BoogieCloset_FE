dc09a5964190d7796b0224d5e64531af4eba25b2
kaeun1008 <128047735+kaeun1008@users.noreply.github.com>
2024-04-29 23:37:24 +0900

    Add files via upload

---
+++ src/components/register/register.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from './register.module.css';
+
+function Register() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.register_text}>Register</h1>
+      <form>
+        <p>Email address</p>
+        <input
+          className={styles.input_style1}
+          type="email"
+          placeholder="email@janesfakedomain.net"
+          required
+        />
+        <p>Password</p>
+        <input
+          className={styles.input_style2}
+          type="password"
+          placeholder="password"
+          required
+        />
+        <p>Password Confirmation</p>
+        <input
+          className={styles.input_style2}
+          type="password"
+          placeholder="password"
+          required
+        />
+        <button className={styles.button_style} type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
